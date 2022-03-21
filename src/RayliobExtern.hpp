0996947108ea0fda73e03ce06c4e80a11362a510
Alexander Griffis <fatalsleep@gmail.com>
2022-03-20 18:15:58 -0700

    Create RayliobExtern.hpp

---
+++ src/RayliobExtern.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef RAYLIB_EXTERN
+#define RAYLIB_EXTERN
+	#ifdef __cplusplus
+	extern "C" {
+		#include <raylib.h>
+		#include <raymath.h>
+
+		#include <extras/rpng.h>
+		#include <extras/rres.h>
+	}
+	#endif
+#endif
